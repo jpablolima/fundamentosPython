8e1221a5cb8ad982b97fca3e3101a1a13bd45b44
João Pablo <jp.pablolima@gmail.com>
2021-02-18 12:52:17 -0300

    python básico

---
+++ python3-do-zero-ao-avancado/env/Scripts/aulas/operadores.py
@@ -0,0 +1,13 @@
+'''
+Operadores
++ ,  - , *, / , //, %, ** 
+
+'''
+
+print('Soma: ', 10+2 )
+print('Subtração: ', 10 - 2 )
+print('Multiplicação: ', 10 * 2 )
+print('Divisão: ', 10/3 )
+print('Divisão inteiro: ', 10 // 2 )
+print('Resto da Divisão: ', 10 % 3 )
+print('Potênciação: ', 10 ** 2 )
---
+++ python3-do-zero-ao-avancado/env/Scripts/aulas/variaveis.py
@@ -0,0 +1,15 @@
+#  Variáveis?
+
+name = 'Pablo'
+age = 29
+height = 1.75
+weight = 89
+older = age > 18
+
+print('Name: ',name)
+print('Age: ',age)
+print('Height:', height)
+print('older: ', older)
+
+imc = weight/(height * height)
+print(name, 'idade: ', age,  'seu imc é ',imc)
