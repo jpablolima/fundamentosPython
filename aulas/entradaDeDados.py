f68703209d73e05caa9398adc91c770ce58b2802
João Pablo <jp.pablolima@gmail.com>
2021-02-19 12:53:15 -0300

    entrada de dados

---
+++ aulas/entradaDeDados.py
@@ -0,0 +1,21 @@
+'''
+    Entrada de Dados -  input
+'''
+
+number1 = int(input('Digite o primiro número: '))
+number2 = int(input('Digite o segundo número: '))
+
+
+soma = number1 + number2
+sub = number1 - number2
+mult = number1 * number2
+div = number1 / number2
+res_div = number1 % number2
+exp = number1 ** number2
+
+print(soma)
+print(sub)
+print(mult)
+print(div)
+print(res_div)
+print(exp)
