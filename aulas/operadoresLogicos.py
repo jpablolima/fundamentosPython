455e53a82ecb279256bfc6dea970aecc35003ba1
João Pablo <jp.pablolima@gmail.com>
2021-02-22 12:54:42 -0300

    operadores

---
+++ aulas/operadoresLogicos.py
@@ -0,0 +1,20 @@
+# nome = 'Pablo'
+
+# if 'h' in nome:
+#     print(f'tem no {nome}')
+# else:
+#     print(f'não tem no {nome}')
+
+
+
+
+user = input('Use: ')
+password = int(input('password: '))
+
+user_db = 'Pablo'
+password_db = 123456
+
+if(user == user_db and password == password_db):
+    print('Logado')
+else:
+    print(f'usuário ou senha incorreto')   
---
+++ aulas/operadoresRelacionaisBooleans.py
@@ -0,0 +1,45 @@
+''' 
+    Operadores Boolens
+    if, elif, else
+'''
+
+# if True:
+#     print('true')
+# elif True:
+#     print('true, True')
+# else:
+#     print('False')
+
+
+
+''' 
+    Operadores Relacionais
+    == > >= < <= !=
+
+'''
+num1 = 10
+num2 = 5
+
+x = num1 == num2
+x = num1 < num2
+x = num1 > num2
+# print(x)
+
+
+# name = 'Pablo'
+# age = 29
+# limite = 18;
+
+# if( age > limite):
+#     print('maior de idade')
+# else:
+#     print('menor de idade')
+
+
+name = input('Nome: ')
+age = int(input('Age: '))
+
+if(age >= 18):
+    print('older')
+else:
+    print('underage')
